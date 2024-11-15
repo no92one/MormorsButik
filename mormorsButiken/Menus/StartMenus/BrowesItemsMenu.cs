f6c60454fc1522fc7b71233b834d68bd4dbde821
unknown <linus.lindroth.92@gmail.com>
2024-11-15 13:13:36 +0100

    Inital commit

---
+++ mormorsButiken/Menus/StartMenus/BrowesItemsMenu.cs
@@ -0,0 +1,49 @@
+﻿namespace mormorsButiken.Menus;
+
+public class BrowesItemsMenu
+{
+    public TopsMenu TopMenu;
+    public BottomsMenu BottomMenu;
+
+    public BrowesItemsMenu()
+    {
+        TopMenu = new TopsMenu();
+        BottomMenu = new BottomsMenu();
+    }
+    
+    public void Start()
+    {
+        bool run = true;
+        while (run)
+        {
+            
+            Console.WriteLine($"\n--- Browes Items ---" +
+                              $"\n" +
+                              $"\n1. Tops" +
+                              $"\n2. Bottoms" +
+                              $"\nB. Back" +
+                              $"\n" );
+            
+            String choice = Console.ReadLine();
+
+            switch (choice.Trim().ToLower())
+            {
+                case "1":
+                    TopMenu.Start();
+                    break;
+                
+                case "2":
+                    BottomMenu.Start();
+                    break;
+                
+                case "b":
+                    run = false;
+                    break;
+                
+                default:
+                    Console.WriteLine("You need to pick 1 - 4.");
+                    break;
+            }
+        }
+    }
+}
---
+++ mormorsButiken/Menus/StartMenus/BrowsItemsMenus/TopsMenu.cs
@@ -0,0 +1,35 @@
+﻿namespace mormorsButiken.Menus;
+
+public class TopsMenu
+{
+    public void Start()
+    {
+        bool run = true;
+        while (run)
+        {
+            
+            Console.WriteLine($"\n--- Tops ---" +
+                              $"\n" +
+                              $"\n1. Items" +
+                              $"\nB. Back" +
+                              $"\n" );
+            
+            String choice = Console.ReadLine();
+
+            switch (choice.Trim().ToLower())
+            {
+                case "1":
+                    Console.WriteLine("Items - WIP");
+                    break;
+                
+                case "b":
+                    run = false;
+                    break;
+                
+                default:
+                    Console.WriteLine("You need to pick 1 - 4.");
+                    break;
+            }
+        }
+    }
+}
